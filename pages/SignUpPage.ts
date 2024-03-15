be87ba8199d14326a09127ddf7935a41a8f19ffb
Pavlo Kuravskyi <pkuravsky@gmail.com>
2024-03-15 10:16:51 +0200

    remove unused imports

--- pages/SignUpPage.ts
+++ pages/SignUpPage.ts
@@ -1,4 +1,4 @@
-import { Page, Locator, expect } from '@playwright/test';
+import { Page, Locator } from '@playwright/test';
 import { faker } from '@faker-js/faker';
 import { BasePage } from './BasePage';
 
--- pages/checkout/BaseCheckoutPage.ts
+++ pages/checkout/BaseCheckoutPage.ts
@@ -1,4 +1,4 @@
-import { Page, Locator, expect } from '@playwright/test';
+import { Page } from '@playwright/test';
 import { BasePage } from '../BasePage';
 
 export class BaseCheckoutPage extends BasePage {
