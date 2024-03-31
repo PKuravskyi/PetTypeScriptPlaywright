d5f0bd39fee94961032ad464582f70b9cda528ac
Pavlo Kuravskyi <pkuravsky@gmail.com>
2024-03-31 17:41:43 +0300

    test

--- tests/visualComparison/arts_visual.spec.ts
+++ tests/visualComparison/arts_visual.spec.ts
@@ -21,7 +21,7 @@ test.describe(
 			await artsPage.verifyPageHasScreenshot();
 		});
 
-		test('Test3 Verify arts page visualisation (descending sorting)', async ({
+		test('Test4 Verify arts page visualisation (descending sorting)', async ({
 			artsPage,
 		}) => {
 			await artsPage.sortBy('price-desc');
